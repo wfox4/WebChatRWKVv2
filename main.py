e935cab3d7a1f5b0d4c57b00a91b2d5f0a61f7b4
wfox4 <117791787+wfox4@users.noreply.github.com>
2023-04-07 13:26:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,32 +18,35 @@ print(
 print("Importing modules...")
 
 import asyncio
-
+import currentcontext
 from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
+import json
 
 import model
+from model import stop_model
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.websocket("/ws")
-async def websocket(ws: WebSocket):
-    loop = asyncio.get_running_loop()
-    await ws.accept()
-
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
     session = {"state": None}
+    loop = asyncio.get_running_loop()
 
     async def reply(id, *, result=None, error=None):
         either = (result is None) is not (error is None)
         assert either, "Either result or error must be set!"
 
         if result is not None:
-            await ws.send_json({"jsonrpc": "2.0", "result": result, "id": id})
+            await websocket.send_json({"jsonrpc": "2.0", "result": result, "id": id})
         elif error is not None:
-            await ws.send_json({"jsonrpc": "2.0", "error": error, "id": id})
+            await websocket.send_json({"jsonrpc": "2.0", "error": error, "id": id})
 
-    def on_progress(id):
+    def on_progress(id, loop):
         def callback(res):
             asyncio.run_coroutine_threadsafe(reply(id, result={"token": res}), loop)
 
@@ -62,34 +65,48 @@ async def websocket(ws: WebSocket):
         return callback
 
     while True:
-        data = await ws.receive_json()
-        if "jsonrpc" not in data or data["jsonrpc"] != "2.0":
-            await reply(
-                data.get("id", None) if type(data) == dict else None,
-                error="invalid message",
-            )
+        data = await websocket.receive_text()
+        print("Received data:", data)
+        # process data as required for your application
+
+        # Add this block to handle stop model request
+        message = json.loads(data)
+        if message.get("action") == "stop_model":
+            stop_model()
+            await websocket.send_text("Model stopped")
+        # End of added block
 
         method, params, id = (
-            data.get("method", None),
-            data.get("params", None),
-            data.get("id", None),
+            message.get("method", None),
+            message.get("params", None),
+            message.get("id", None),
         )
+
+        if message.get("action") == "update_context":
+            new_context = message.get("context", None)
+            if new_context is not None:
+                currentcontext.userscontext = new_context.strip()
+            continue
 
         if method == "chat":
             text = params.get("text", None)
             if text is None:
                 await reply(id, error="text is required")
 
+
+            
+
             await loop.run_in_executor(
                 None,
                 model.chat,
                 session["state"],
                 text,
-                on_progress(id),
+                on_progress(id, loop),
                 on_done(text),
             )
         else:
             await reply(id, error=f"invalid method '{method}'")
+
 
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
