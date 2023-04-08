ee0c9bdf5b73971b094f16f88d98343cbff86269
wfox4 <117791787+wfox4@users.noreply.github.com>
2023-04-08 16:19:19 -0400

    Update index.js

--- static/index.js
+++ static/index.js
@@ -2,74 +2,9 @@
     const messages = {};
     let isReady = false;
 
-    let historybox;
 
-        // Connect to websocket
-    const ws = new WebSocket(
-        location.protocol.replace("http", "ws") + "//" + location.host + "/ws"
-    );
 
-    const saveConversationHistory = (filename, conversationHistory) => {
-        localStorage.setItem(filename, JSON.stringify(conversationHistory));
-    };
 
-    const renderMessage = (historybox, id, from, message) => {
-        if (!message) {
-            return;
-        }
-        messages[id] = message;
-
-        const div = document.createElement("div");
-        div.id = id;
-        div.className = "message";
-
-        const tname = document.createElement("h4");
-        tname.innerText = from;
-        div.appendChild(tname);
-
-        const txt = document.createElement("div");
-        txt.innerHTML = marked.parse(message);
-        txt.className = "messagecontent";
-        div.appendChild(txt);
-
-        historybox.appendChild(div);
-    };
-    const makeId = () =>
-        (Date.now().toString(36) + Math.random().toString(36)).replace(".", "");
-
-    // Add a new variable to keep track of the active log filename
-    let activeLogFilename = null;
-
-    const switchActiveLog = (historybox, filename) => {
-        // Save the current conversation before switching logs
-        if (activeLogFilename) {
-            const currentConversation = Array.from(historybox.querySelectorAll(".message")).map((messageElement) => {
-                const from = messageElement.querySelector("h4").innerText;
-                const message = messageElement.querySelector(".messagecontent").innerText;
-                return {
-                    from,
-                    message
-                };
-            });
-
-            saveConversationHistory(activeLogFilename, currentConversation);
-        }
-
-        // Clear the current conversation history
-
-        historybox.innerHTML = "";
-
-        // Load the conversation history from local storage
-        const savedConversationHistory = localStorage.getItem(filename);
-        const conversationHistory = savedConversationHistory ? JSON.parse(savedConversationHistory) : [];
-
-        conversationHistory.forEach((entry) => {
-            renderMessage(historybox, makeId(), entry.from, entry.message);
-        });
-
-        // Update the active log filename
-        activeLogFilename = filename;
-    };
 
 
 
@@ -78,9 +13,78 @@
         const conversationLogsElement = document.querySelector("#conversation-logs");
         const chatbox = document.querySelector("#chatbox");
         const chatform = document.querySelector("#chatform");
-        historybox = document.querySelector("#history");
+        const historybox = document.querySelector("#history");
 
         const contextbox = document.getElementById("contextbox");
+
+        const ws = new WebSocket(
+            location.protocol.replace("http", "ws") + "//" + location.host + "/ws"
+        );
+
+        const saveConversationHistory = (filename, conversationHistory) => {
+            localStorage.setItem(filename, JSON.stringify(conversationHistory));
+        };
+
+        const renderMessage = (id, from, message) => {
+            messages[id] = message;
+
+            console.log("renderMessage", id, from, message);
+            messages[id] = message;
+
+            const div = document.createElement("div");
+            div.id = id;
+            div.className = "message";
+
+            const tname = document.createElement("h4");
+            tname.innerText = from;
+            div.appendChild(tname);
+
+            const txt = document.createElement("div");
+            txt.innerHTML = marked.parse(message);
+            txt.className = "messagecontent";
+            div.appendChild(txt);
+
+            historybox.appendChild(div);
+        };
+
+        const makeId = () =>
+            (Date.now().toString(36) + Math.random().toString(36)).replace(".", "");
+
+        // Add a new variable to keep track of the active log filename
+        let activeLogFilename = null;
+
+        const switchActiveLog = (filename) => {
+            // Save the current conversation before switching logs
+            if (activeLogFilename) {
+                const currentConversation = Array.from(historybox.querySelectorAll(".message")).map((messageElement) => {
+                    const from = messageElement.querySelector("h4").innerText;
+                    const message = messageElement.querySelector(".messagecontent").innerText;
+                    return {
+                        from,
+                        message
+                    };
+                });
+
+                saveConversationHistory(activeLogFilename, currentConversation);
+            }
+
+            // Clear the current conversation history
+
+
+
+            // Load the conversation history from local storage
+            const savedConversationHistory = localStorage.getItem(filename);
+            const conversationHistory = savedConversationHistory ? JSON.parse(savedConversationHistory) : [];
+
+            conversationHistory.forEach((entry) => {
+                renderMessage(makeId(), entry.from, entry.message);
+            });
+
+            // Update the active log filename
+            activeLogFilename = filename;
+        };
+
+
 
         contextbox.addEventListener("input", (event) => {
             const context = contextbox.value.trim();
@@ -98,23 +102,56 @@
             await ws.send(JSON.stringify(payload));
         }
 
+        const loadLogsFromLocalStorage = () => {
+            const logKeys = Object.keys(localStorage);
+            const logs = [];
+
+            logKeys.forEach((key) => {
+                if (key.startsWith("new-log-")) {
+                    logs.push(key);
+                }
+            });
+
+            return logs;
+        };
+
         const createConversationLog = (filename) => {
             const logItem = document.createElement("div");
             logItem.className = "log-item";
 
-            const logFilename = document.createElement("span");
+            const filenameContainer = document.createElement("div");
+            filenameContainer.className = "filename-container";
+
+            const logFilename = document.createElement("div");
             logFilename.className = "log-filename";
+            logFilename.style.display = "flex";
+            logFilename.style.alignItems = "center";
             logFilename.innerText = filename;
 
-            logItem.appendChild(logFilename);
-            logItem.addEventListener("click", () => {
-                switchActiveLog(historybox, filename);
-            });
+            const editButtonContainer = document.createElement("div");
+            editButtonContainer.className = "edit-btn-container";
 
             const editButton = document.createElement("button");
             editButton.className = "log-edit-btn";
             editButton.innerText = "Edit";
-            logItem.appendChild(editButton);
+            editButtonContainer.appendChild(editButton);
+
+            const deleteButtonContainer = document.createElement("div");
+            deleteButtonContainer.className = "delete-btn-container";
+
+            const deleteButton = document.createElement("button");
+            deleteButton.className = "log-delete-btn";
+            deleteButton.innerHTML = '<img src="trashcan.png">';
+            deleteButton.setAttribute("data-filename", filename);
+            deleteButtonContainer.appendChild(deleteButton);
+
+            filenameContainer.appendChild(logFilename);
+            editButtonContainer.appendChild(editButton);
+            deleteButtonContainer.appendChild(deleteButton);
+
+            logItem.appendChild(filenameContainer);
+            logItem.appendChild(editButtonContainer);
+            logItem.appendChild(deleteButtonContainer);
 
             editButton.addEventListener("click", () => {
                 const newFilename = prompt("Enter a new name for the log:", filename);
@@ -125,15 +162,42 @@
                 }
             });
 
+            deleteButton.addEventListener("click", () => {
+                const confirmDelete = confirm(`Are you sure you want to delete ${filename}?`);
+                if (confirmDelete) {
+                    localStorage.removeItem(filename);
+                    caches.open("conversation-history").then((cache) => {
+                        cache.delete(filename);
+                    });
+                    logItem.remove();
+                    console.log(`Deleting file: ${filename}`);
+                    if (activeLogFilename === filename) {
+                        activeLogFilename = null;
+                        historybox.innerHTML = "";
+                    }
+                }
+            });
+
             return logItem;
         };
 
-        const exampleLogs = []; // Replace this with your actual log data
+
+
+        const exampleLogs = loadLogsFromLocalStorage();
+
+        if (exampleLogs.length === 0) {
+            // Add a default conversation log if no logs exist
+            localStorage.setItem("default-log", JSON.stringify([]));
+            exampleLogs.push("default-log");
+        }
 
         exampleLogs.forEach((log) => {
             const logItem = createConversationLog(log);
             conversationLogsElement.appendChild(logItem);
         });
+
+        activeLogFilename = exampleLogs[0];
+        switchActiveLog(historybox, activeLogFilename);
 
         marked.setOptions({
             highlight: function(code, lang) {
@@ -146,32 +210,6 @@
             breaks: true, // added line break option for markdown
         });
 
-        const renderMessage = (id, from, message) => {
-            messages[id] = message;
-
-            const div = document.createElement("div");
-            div.id = id;
-            div.className = "message";
-
-            const tname = document.createElement("h4");
-            tname.innerText = from;
-            div.appendChild(tname);
-
-            const txt = document.createElement("div");
-            txt.innerHTML = marked.parse(message);
-            txt.className = "messagecontent";
-
-            if (from === "User") {
-                txt.classList.add("user-message");
-            } else {
-                txt.classList.add("bot-message");
-            }
-
-            div.appendChild(txt);
-
-            historybox.appendChild(div);
-        };
-
 
         const appendMessage = (id, message) => {
             messages[id] += message;
@@ -182,16 +220,18 @@
 
             // Append to the p
             const p = document.querySelector("#" + id + " > .messagecontent");
-            p.innerHTML = marked.parse(markdown);
+            if (p !== null) {
+                p.innerHTML = marked.parse(markdown);
 
-            // Scroll the history box
-            historybox.scrollTo({
-                behavior: "smooth",
-                top: historybox.scrollHeight,
-                left: 0,
-            });
+
+                // Scroll the history box
+                historybox.scrollTo({
+                    behavior: "smooth",
+                    top: historybox.scrollHeight,
+                    left: 0,
+                });
+            }
         };
-
 
 
 
@@ -227,6 +267,8 @@
             // Save the current conversation to local storage
             const newLogFilename = "new-log-" + Date.now() + ".txt";
             saveConversationHistory(newLogFilename, currentConversation);
+            exampleLogs.push(newLogFilename);
+            localStorage.setItem("logs", JSON.stringify(exampleLogs));
 
             // Add the new log to the conversation logs list
             const newLogItem = createConversationLog(newLogFilename);
@@ -235,21 +277,23 @@
 
 
         const sendMessage = async (message) => {
-			isReady = false;
+            isReady = false;
 
             if (isFirstMessage) {
                 saveCurrentConversation();
                 isFirstMessage = false;
             }
             // Generate an ID for the response
-            respid = makeId();
 
+            respid = makeId();
             // Add message to the page
             renderMessage(makeId(), "User", message);
             renderMessage(respid, "ChatRWKV", "");
 
+
+
             // Send message to server
-            ws.send(
+            await ws.send(
                 JSON.stringify({
                     jsonrpc: "2.0",
                     method: "chat",
@@ -259,11 +303,6 @@
                     id: respid,
                 })
             );
-
-            // Reset the value of the chatbox
-            chatbox.value = "";
-            chatform.focus();
-            chatform.reset();
         };
 
         const onSubmit = () => {
@@ -275,19 +314,15 @@
             chatform.reset();
         };
 
-		chatform.addEventListener("submit", (e) => {
-			e.preventDefault();
-			const message = chatbox.value.trim();
-			if (message !== "") {
-				sendMessage(message);
-				chatbox.value = "";
-			}
-			chatform.focus();
-			chatform.reset();
-		});
+        chatform.addEventListener("submit", (e) => {
+            e.preventDefault();
+            onSubmit();
+        });
+
         chatbox.addEventListener("keydown", (e) => {
             if (e.key == "Enter" && !e.shiftKey) onSubmit();
         });
+
         const updateLogFilename = (oldName, newName) => {
             // Send a request to your server to update the log filename
             // Implement the server-side logic to handle the update request
@@ -301,8 +336,11 @@
                 }));
             }
         };
+
         stopButton.addEventListener("click", stopModel);
+
         window.stopModel = stopModel;
+
     });
 
 })();
