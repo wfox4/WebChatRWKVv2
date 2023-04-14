4552cdce8c3924345abcf07be4ff545c2385c0da
wfox4 <117791787+wfox4@users.noreply.github.com>
2023-04-13 20:56:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,19 @@ async def stop_model_route(background_tasks: BackgroundTasks):
     print("Stop model endpoint called")
     return {"status": "Model stopping"}
 
+@app.get("/your_context_endpoint")
+async def chat_current_context():
+    return Response(content=currentcontext.userscontext, media_type="text/plain")
 
+@app.put("/toggle_chatmode")
+async def toggle_chatmode():
+    model.chatmode = not model.chatmode
+    model.load_context()
+    return {"status": "success", "chatmode": model.chatmode}
+
+@app.get("/get_chatmode")
+async def get_chatmode():
+    return {"chatmode": model.chatmode}
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -57,14 +69,12 @@ async def websocket_endpoint(websocket: WebSocket):
     session = {"state": None}
     loop = asyncio.get_running_loop()
 
-
-
     async def reply(id, *, result=None, error=None):
         either = (result is None) is not (error is None)
         assert either, "Either result or error must be set!"
 
         if result is not None:
-            await websocket.send_json({"jsonrpc": "2.0", "result": result, "id": id})        
+            await websocket.send_json({"jsonrpc": "2.0", "result": result, "id": id})
         elif error is not None:
             await websocket.send_json({"jsonrpc": "2.0", "error": error, "id": id})
 
@@ -74,15 +84,38 @@ async def websocket_endpoint(websocket: WebSocket):
 
         return callback
 
+    def remove_unwanted_strings(text: str):
+        items_to_remove = ["\nAlice:", "\nAlice: ", "Alice:", "Alice: ", " 1. A: ", " * "]
+        for item in items_to_remove:
+            if text.startswith(item):
+                text = text.replace(item, "", 1)
+                break
+        return text
+
     def on_done(input):
         def callback(result):
-            print("--- input ---")
-            print(input)
-            print("--- output ---")
-            print(result["output"])
-            print("---")
-
+            output = result["output"]
+            
             session["state"] = result["state"]
+            if model.chatmode:
+                output = remove_unwanted_strings(output)
+                if output.endswith("\nBob:") or output.endswith("\nBob: "):
+                    output = output[:-len("\nBob:")]
+                elif output.endswith("Bob:") or output.endswith("Bob: "):
+                    output = output[:-len("Bob:")]
+                print("Bob: " + input + "\n\n")
+                print("Alice:" + output)
+
+                if currentcontext.userscontext == "":
+                    combined_message = f"Bob: {input}\n\nAlice:{output}"
+                else:
+                    combined_message = f"\n\nBob: {input}\n\nAlice:{output}"
+
+                currentcontext.userscontext = currentcontext.userscontext + combined_message
+                
+
+                # Notify the frontend to update the context box
+                asyncio.run_coroutine_threadsafe(websocket.send_text(json.dumps({"action": "update_context"})), loop)
 
 
         return callback
@@ -101,7 +134,6 @@ async def websocket_endpoint(websocket: WebSocket):
 
         if "number" in message:
             set_number(int(message["number"]))
-
 
         method, params, id = (
             message.get("method", None),
@@ -132,19 +164,20 @@ async def websocket_endpoint(websocket: WebSocket):
                 on_progress(id, loop),
                 lambda res: on_done(text)(res),
             )
-    
-    # Check if the result dictionary contains the expected key
+
+            # Check if the result dictionary contains the expected key
             if "output" in result:
                 output = result["output"]
             else:
                 # Use a default value if the key is not present
                 output = "Sorry, I didn't understand that."
 
-    # Save the model's response
+            # Save the model's response
             save_log('conversation_log.txt', f'Model: {output}\n')
-    
+
         else:
             await reply(id, error=f"invalid method '{method}'")
+
 
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
